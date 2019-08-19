a7923462ac637e7a1353f569fc7434afcececf13
ThanhThanh2911 <laithanhthanh2911@gmail.com>
2019-08-19 16:57:38 +0700

    .

--- my.js
+++
@@ -1,5 +0,0 @@
-document.write("xin chao");
-
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
-}
--- sudungtoantu.js
+++
@@ -1,5 +0,0 @@
-var inputWidth = +prompt('nhap chieu rong');
-var inputHeight = +prompt('nhap chieu dai');
-var dientich = inputWidth * inputHeight;
-
-document.write('dien tich =' +dientich);
