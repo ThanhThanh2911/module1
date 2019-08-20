a9a898f2c358bb21a18473cfc1286bf335e5a2d7
ThanhThanh2911 <laithanhthanh2911@gmail.com>
2019-08-20 09:07:16 +0700

    .

---
+++ Biến kiểu dữ liệu và toán tử/sudungtoantu.js
@@ -0,0 +1,7 @@
+var inputWidth = +prompt('nhap chieu rong');
+var inputHeight = +prompt('nhap chieu dai');
+var dientich = inputWidth * inputHeight;
+
+document.write('Chiều dài: ' +inputWidth);
+document.write('Chiều rộng: ' +inputHeight);
+document.write('Diện tích =' +dientich);
